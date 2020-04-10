52393aaa67ee64828ff11c35f26bcda9ebabf407
lakshyaakar <lakshyaakar007@gmail.com>
2020-04-10 13:53:54 +0530

    Passport

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var User = require('./models/user');
+var User = require('./models/users.js');
 
 
 passport.use(new LocalStrategy(User.authenticate()));
